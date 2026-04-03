d2bd0a8c52c15f37b496930300d1772c496d596e
DGXEONHOZOOMD <hozoolinux@gmail.com>
2026-04-03 08:15:00 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,5 @@
+
+global.owner = ["6283848833500"] // ubah jadi nomor mu
+global.name = "someone"
+global.frch = ["c97d9437c6b686e051e7bb4adc59e68a6c34fee821ef0ba859a54950ad344bf4"// Dapatkan apikey di https://asitha.top
+]
